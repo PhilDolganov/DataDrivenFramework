ddb3066b0f55130192ea2fcbd50fbdfa81395f6c
Phil Dolganov <portnovtest@gmail.com>
2018-05-12 20:33:50 -0700

    2.5 commit. Lecture 16 Section 1

---
+++ src/test/java/com/w2a/testcases/OpenAccountTest.java
@@ -0,0 +1,25 @@
+package com.w2a.testcases;
+
+import com.w2a.base.TestBase;
+import com.w2a.utilities.TestUtil;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+public class OpenAccountTest extends TestBase {
+
+    @Test(dataProviderClass = TestUtil.class,dataProvider = "dp")
+    public void openAccountTest(String customer, String currency) throws InterruptedException {
+
+        click("openaccount_CSS");
+        select("customer_CSS",customer);
+        select("currency_CSS",currency);
+        click("process_CSS");
+        Thread.sleep(2000);
+        Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+        alert.accept();
+    }
+}
